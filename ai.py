c9a8c1c22a2881a705f09bfb382a09e610907504
ASH <78680782+And-oss@users.noreply.github.com>
2024-04-20 22:59:29 +0300

    added ai

---
+++ ai.py
@@ -0,0 +1,41 @@
+api = "AQVN0TlD2PWbz_NsY_fVd4X13lp0g3xz8GIvz_cv"
+id = "b1go51mogaeuh0gh7qbc"
+import requests
+
+prompt = {
+    "modelUri": f"gpt://{id}/yandexgpt-lite",
+    "completionOptions": {
+        "stream": False,
+        "temperature": 0.6,
+        "maxTokens": "2000"
+    },
+    "messages": [
+        {
+            "role": "system",
+            "text": "Ты ассистент дроид, способный помочь в галактических приключениях."
+        },
+        {
+            "role": "user",
+            "text": "Привет, Дроид! Мне нужна твоя помощь, чтобы узнать больше о Силе. Как я могу научиться ее использовать?"
+        },
+        {
+            "role": "assistant",
+            "text": "Привет! Чтобы овладеть Силой, тебе нужно понять ее природу. Сила находится вокруг нас и соединяет всю галактику. Начнем с основ медитации."
+        },
+        {
+            "role": "user",
+            "text": "Хорошо, а как насчет строения светового меча? Это важная часть тренировки джедая. Как мне создать его?"
+        }
+    ]
+}
+
+
+url = "https://llm.api.cloud.yandex.net/foundationModels/v1/completion"
+headers = {
+    "Content-Type": "application/json",
+    "Authorization": f"Api-Key {api}"
+}
+
+response = requests.post(url, headers=headers, json=prompt)
+result = response.text
+print(result)
